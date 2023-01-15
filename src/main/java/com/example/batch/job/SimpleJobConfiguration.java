a5c556555bf2a6c1d51c22c4b83c1f861b993cf6
swh <tjdngur22@gmail.com>
2023-01-15 12:51:42 +0900

    "save"

---
+++ src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.batch.job;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration//  @Configuration Spring Batch의 모든 Job은 @Configuration으로 등록해서 사용합니다.
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
+    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+
+    @Bean
+    public Job simpleJob() {
+        //jobBuilderFactory.get("simpleJob") simpleJob 이란 이름의 Batch Job을 생성합니다.
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        //simpleStep1 이란 이름의 Batch Step을 생성합니다.
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    //Step 안에서 수행될 기능들을 명시합니다.
+                    //Tasklet 하나와 Reader & Processor & Writer 한 묶음이 같은 레벨입니다.
+                    log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
